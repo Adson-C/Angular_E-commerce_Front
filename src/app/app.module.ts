6f05f79f47cb882b97eb9b22397ce4776a29775a
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-10-17 23:54:53 -0300

    Procura para Produtos Categoria , Menu de categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 import { Routes, RouterModule} from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+
 
 
 const routes: Routes = [
@@ -20,7 +22,8 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductCategoryMenuComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
